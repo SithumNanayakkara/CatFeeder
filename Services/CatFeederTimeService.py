4a95a419772e1a6926cbacaf0239bbb97d22e983
Sithum <115341061+SithumNanayakkara@users.noreply.github.com>
2023-08-22 18:52:58 +1200

    Hopper_Spin_Time changed to Servo_Open_Time

--- Services/CatFeederTimeService.py
+++ Services/CatFeederTimeService.py
@@ -22,7 +22,7 @@ configParser.read(configFilePath)
 secondDelay = configParser.get('CatFeederConfig', 'Seconds_Delay_Between_Schedule_Checks')
 LOG_TimeService_FILENAME = configParser.get('CatFeederConfig', 'Log_TimeService_Filename')
 hopperGPIO = str(configParser.get('CatFeederConfig', 'Servo_GPIO_Pin'))
-hopperTime = str(configParser.get('CatFeederConfig', 'Hopper_Spin_Time'))
+hopperTime = str(configParser.get('CatFeederConfig', 'Servo_Open_Time'))
 motionVideoDirPath = str(configParser.get('CatFeederConfig', 'Motion_Video_Dir_Path'))
 nowMinusXDays = str(configParser.get('CatFeederConfig', 'Number_Days_Of_Videos_To_Keep'))
 
--- app.py
+++ app.py
@@ -27,7 +27,7 @@ configParser.read('/var/www/CatFeeder/app.cfg')
 # Read in config variables
 SECRETKEY = str(configParser.get('CatFeederConfig', 'Secretkey'))
 hopperGPIO = str(configParser.get('CatFeederConfig', 'Servo_GPIO_Pin'))
-hopperTime = str(configParser.get('CatFeederConfig', 'Hopper_Spin_Time'))
+hopperTime = str(configParser.get('CatFeederConfig', 'Servo_Open_Time'))
 DB = str(configParser.get('CatFeederConfig', 'Database_Location'))
 latestXNumberFeedTimesValue = str(configParser.get('CatFeederConfig', 'Number_Feed_Times_To_Display'))
 upcomingXNumberFeedTimesValue = str(configParser.get('CatFeederConfig', 'Number_Scheduled_Feed_Times_To_Display'))
